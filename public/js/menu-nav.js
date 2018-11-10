defda94ff05c9d2975cbad019f9f140f35178139
Mihail Moscalu <mmoskalu@gmail.com>
2018-11-11 00:41:44 +0200

    menu close btn

--- public/js/menu-nav.js
+++ public/js/menu-nav.js
@@ -1,11 +1,22 @@
 let menuNav = document.getElementById('menu-id');
 let menuBtn = document.getElementById('menu-btn-id');
-
-
+let navItem = document.getElementsByClassName('nav-item');
 
 menuBtn.onclick =  function (e) {
+
     e.preventDefault();
     menuBtn.classList.toggle('menu-btn-active');
     menuNav.classList.toggle('menu-nav-active');
-    console.log('click');
+
 };
+
+for (let i = 0; i < navItem.length; i++) {
+
+    navItem[i].onclick = function (e) {
+        menuNav.classList.remove('menu-nav-active');
+        menuBtn.classList.toggle('menu-btn-active');
+
+    };
+
+}
+
